25b8721f0c88b57fd82671a746c158ec7e8b6060
Maks996699 <maks996699@gmail.com>
2018-04-24 17:54:10 +0300

    add 7.1

---
+++ 7.1/Main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+const int MAX = 20;
+
+void reversit(char[MAX]);
+
+int main()
+{
+	char str[MAX];
+	cin.get(str, MAX);
+
+	return 0;
+}
+
+void reversit(char arr[MAX])
+{
+	for (int i = 0; i < length; i++)
+	{
+
+	}
+}
