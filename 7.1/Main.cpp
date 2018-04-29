a3db6f43daa3e8400cd3d79f56f688fcae611a04
Maks <Maks@MAKSIMUS>
2018-04-29 23:55:17 +0300

    fix 7.1

--- 7.1/Main.cpp
+++ 7.1/Main.cpp
@@ -5,20 +5,25 @@ using namespace std;
 
 const int MAX = 20;
 
-void reversit(char[MAX]);
+void reversit(char[]);
 
 int main()
 {
 	char str[MAX];
 	cin.get(str, MAX);
+	reversit(str);
+	cout << str << endl;
 
 	return 0;
 }
 
-void reversit(char arr[MAX])
+void reversit(char arr[])
 {
-	for (int i = 0; i < length; i++)
+	char temp;
+	for (int i = 0, j = (strlen(arr) - 1); i < (strlen(arr) / 2); i++ , j--)
 	{
-
+		temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
 	}
 }
