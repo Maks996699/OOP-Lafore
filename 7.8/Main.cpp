5a4c02cf920446ea7d54ac2369604b7f1f418d78
Maks <Maks@MAKSIMUS>
2018-05-06 15:30:03 +0300

    fix 7.8

--- 7.8/Main.cpp
+++ 7.8/Main.cpp
@@ -7,6 +7,8 @@ class SafeArray
 public:
 	SafeArray();
 	~SafeArray();
+	void putel(int index, int value);
+	int getel(int index);
 
 private:
 	static int const LIMIT = 20;
@@ -25,4 +27,16 @@ int main()
 {
 
 	return 0;
+}
+
+void SafeArray::putel(int index, int value)
+{
+	if ((index < (LIMIT - 1)) && (index > 0))
+	{
+		arr[index] = value;
+	}
+	else
+	{
+		cout << "error: Bad index" << endl;
+	}
 }
