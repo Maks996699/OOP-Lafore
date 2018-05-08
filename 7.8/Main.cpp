6802dda3b7c45687227204a36068673a5ec99fb2
Maks996699 <maks996699@gmail.com>
2018-05-08 11:35:57 +0300

    end 7.8

--- 7.8/Main.cpp
+++ 7.8/Main.cpp
@@ -25,7 +25,10 @@ SafeArray::~SafeArray()
 
 int main()
 {
-
+	SafeArray sa1;
+	int temp = 12345;
+	sa1.putel(7, temp);
+	cout << sa1.getel(7);
 	return 0;
 }
 
@@ -34,6 +37,18 @@ void SafeArray::putel(int index, int value)
 	if ((index < (LIMIT - 1)) && (index > 0))
 	{
 		arr[index] = value;
+	}
+	else
+	{
+		cout << "error: Bad index" << endl;
+	}
+}
+
+int SafeArray::getel(int index)
+{
+	if ((index < (LIMIT - 1)) && (index > 0))
+	{
+		return arr[index];
 	}
 	else
 	{
