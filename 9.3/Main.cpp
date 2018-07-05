4e2d5d76dd63546b63cdd74246d1da7f147e522c
Maks <Maks@MAKS-PC>
2018-07-05 17:54:33 +0300

    fix

--- 9.3/Main.cpp
+++ 9.3/Main.cpp
@@ -81,9 +81,91 @@ void Sales::putdata()
 	}
 }
 
+class Book : public Publication, public Sales
+{
+public:
+	Book();
+	Book(int, string, float);
+	~Book();
+	void getdata();
+	void putdata();
+private:
+	int number_of_page;
+};
+
+Book::Book() : Publication(), Sales(), number_of_page(0)
+{
+}
+
+Book::Book(int nop, string nm, float pr) : Publication(nm , pr), Sales() ,number_of_page(nop)
+{
+}
+
+Book::~Book()
+{
+}
+
+void Book::getdata()
+{
+	cout << "Number of page: " << number_of_page << endl;
+	Sales::getdata();
+}
+
+void Book::putdata()
+{
+	cout << "Input number of page: ";
+	cin >> number_of_page;
+	Sales::putdata();
+}
+
+class Type : public Publication, public Sales
+{
+public:
+	Type();
+	Type(float, string, float);
+	~Type();
+	void getdata();
+	void putdata();
+
+private:
+	float time_of_min;
+};
+
+Type::Type() : Publication(), Sales(), time_of_min(0)
+{
+}
+
+Type::Type(float tom, string nm, float pr) : Publication(nm, pr), Sales(), time_of_min(tom)
+{
+}
+
+Type::~Type()
+{
+}
+
+void Type::getdata()
+{
+	cout << "Time of minute: " << time_of_min << endl;
+	Sales::getdata();
+}
+
+void Type::putdata()
+{
+	cout << "Input time of minute: ";
+	cin >> time_of_min;
+	Sales::putdata();
+}
+
 int main()
 {
-	
+	Book test1;
+	Type test2;
+	test1.getdata();
+	test2.getdata();
+	test1.putdata();
+	test2.putdata();
+	test1.getdata();
+	test2.getdata();
 
 
 	return 0;
